1d13a902b6e5dcedde47ca295dce6d8df27b90d3
Keysi Jones <keysijones01@gmail.com>
2021-05-04 19:40:00 -0300

    "populando base de dados fake"

--- src/main/java/com/keysijones/cadastros/controller/CadastrosController.java
+++ src/main/java/com/keysijones/cadastros/controller/CadastrosController.java
@@ -14,10 +14,32 @@ public class CadastrosController {
 
     @GetMapping("/consultar-cadastros")
     public List<Pessoa> consultarCadastros() {
-        Pessoa pessoa = new Pessoa("Keysi Jones", 24, "Developer");
 
-        List<Pessoa> list = new ArrayList<Pessoa>();
+        Pessoa pessoa = new Pessoa("Keysi Jones", 24, "Developer");
+        Pessoa pessoa1 = new Pessoa("Lauren Viana", 32, "PO");
+        Pessoa pessoa2 = new Pessoa("Eduardo Prado", 27, "QA");
+        Pessoa pessoa3 = new Pessoa("Lucas Silveira", 25, "Tester");
+        Pessoa pessoa4 = new Pessoa("Jonathan James", 29, "BO");
+        Pessoa pessoa5 = new Pessoa("J.K Rowling", 18, "CEO");
+        Pessoa pessoa6 = new Pessoa("Harry Estilos", 34, "Escritor");
+        Pessoa pessoa7 = new Pessoa("Dan Bon Jams", 37, "Frontend Especialist");
+        Pessoa pessoa8 = new Pessoa("Mickey Jaggy", 35, "Backend Developer");
+        Pessoa pessoa9 = new Pessoa("Jon Bones James", 42, "Scrum Master");
+        Pessoa pessoa10 = new Pessoa("Mano Brownie", 19, "Agilista");
+
+        List<Pessoa> list = new ArrayList<>();
+
         list.add(pessoa);
+        list.add(pessoa1);
+        list.add(pessoa2);
+        list.add(pessoa3);
+        list.add(pessoa4);
+        list.add(pessoa5);
+        list.add(pessoa6);
+        list.add(pessoa7);
+        list.add(pessoa8);
+        list.add(pessoa9);
+        list.add(pessoa10);
 
         return list;
     }
